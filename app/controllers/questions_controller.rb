1199a2507661e9aaa1a08d279b85ac27b3945c7e
yue ying <sarahyingyue1994@gmail.com>
2023-05-13 09:59:06 +0200

    stupid coaching

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,5 +4,12 @@ class QuestionsController < ApplicationController
 
   def answer
     @question_input = params[:question]
+    if @question_input == "I am going to work"
+      @result = "Great"
+    elsif @question_input.end_with?("?")
+      @result = "Silly question, get dressed and go to work!"
+    else
+      @result = "I don't care, get dressed and go to work!"
+    end
   end
 end
